0cfff565e156390db9b5bcb803edf551f7eeeb5c
Yurii Polych <bighunt0@gmail.com>
2023-04-05 17:28:40 +0300

    add tables to models.py

---
+++ 31/flask-project/robot_app/models.py
@@ -0,0 +1,27 @@
+from robot_app import db
+from datetime import datetime
+
+
+# Task 36.4 ==========================================
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+
+class Purchase(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    user = db.relationship('User')
+    book = db.relationship('Book')
+    date = db.Column(db.String, default=datetime.utcnow)
