212fa43d51d6c7f99e1810ef05a715b8d7fdf0b0
Yurii Polych <bighunt0@gmail.com>
2023-05-04 14:24:46 +0300

    add the solution to task 44.2

--- django/django_project/settings.py
+++ django/django_project/settings.py
@@ -86,6 +86,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # REST Framework custom settings
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
 
