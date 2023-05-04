de91e9078b0b314cb01513949f587ecc9b1ddd0d
Yurii Polych <bighunt0@gmail.com>
2023-05-04 16:26:35 +0300

    add the solution to task 44.3

--- django/django_project/settings.py
+++ django/django_project/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 5,
 }
 
 # Database
--- django/user/views.py
+++ django/user/views.py
@@ -2,8 +2,16 @@ from user.models import User
 
 from rest_framework.viewsets import ModelViewSet
 from user.serializers import UserSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
+class UserPagination(PageNumberPagination):
+    pass
 
 
 class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.get_queryset().order_by('id')
     serializer_class = UserSerializer
+
+    pagination_class = UserPagination
+    UserPagination.page_size = 10
