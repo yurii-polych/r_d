4da85bcbade023b8736364d9d4f491ce4aaa4fb1
Yurii Polych <bighunt0@gmail.com>
2023-05-05 18:32:23 +0300

    add the solving to task 45.4

--- django/user/tasks.py
+++ django/user/tasks.py
@@ -1,7 +1,15 @@
 from celery import shared_task
 
+from purchase.models import Purchase
+
 
 @shared_task
 def print_any_text():
     text = 'This function prints any text.'
     print(text)
+
+
+@shared_task(bind=False)
+def print_purchases_count(user_id):
+    purchases_count = Purchase.objects.filter(user_id=f'{user_id}').count()
+    print(purchases_count)
